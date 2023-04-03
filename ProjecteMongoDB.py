fe11aa7f4874bbbdbf5bca13773f47665a2941b9
Pau Fuentes <android2002fuentes@gmail.com>
2023-04-03 20:50:06 +0200

    Canvis per al nou excel

--- ProjecteMongoDB.py
+++ ProjecteMongoDB.py
@@ -44,9 +44,9 @@ import pandas as pd
 
 # leer el archivo CSV
 xls = pd.ExcelFile('Dades (1).xlsx')
-Editorial = pd.read_excel(xls,'Colleccions-Publicacions', usecols=[0,1,2,3])
-Colleccio = pd.read_excel(xls,'Colleccions-Publicacions', usecols=[4,5,6,7,8,9,10,11,12,13,14])
-Publicacio = pd.read_excel(xls,'Colleccions-Publicacions', usecols=[15,16,17,18,19,20,21,22,23,24])
+Editorial = pd.read_excel(xls,'Editorial')
+Colleccio = pd.read_excel(xls,'Colleccions')
+Publicacio = pd.read_excel(xls,'Publicacions')
 Personatges = pd.read_excel(xls,'Personatges')
 Artistes = pd.read_excel(xls,'Artistes')
 
